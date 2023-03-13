e2c6fc7f36857fd1a364cebe971dbc4a77d89c72
Ismael Barea Insua <ismael.barea@unit4.com>
2023-03-13 16:46:50 +0100

    Minimal Setup for API

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+using System.Runtime.ExceptionServices;
 
 namespace SuperHeroAPI.Controllers
 {
@@ -7,6 +9,22 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHeroes()
+        {
+            var superHeroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
 
+            return Ok(superHeroes);
+        }
     }
 }
